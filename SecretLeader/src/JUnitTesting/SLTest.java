d6e064549ade5baad18c380bf8c89899d5fe3a80
brewer-slack <bslack@zagmail.gonzaga.edu>
2017-04-23 16:22:22 -0700

    testing package

---
+++ SecretLeader/src/JUnitTesting/SLTest.java
@@ -0,0 +1,14 @@
+package JUnitTesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SLTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
