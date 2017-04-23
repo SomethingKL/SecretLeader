7f041f91ea3da2d588381978c1792a78e83d7f8e
Jacob Krantz <Jacob@Jacob-PC>
2017-04-23 00:49:15 -0700

    A colored button class for the main menu submission. Waiting for Max's update to push the instantiation with main menu.

---
+++ SecretLeader/src/framework/ColorButton.java
@@ -0,0 +1,54 @@
+package framework;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JButton;
+
+public class ColorButton extends JButton {
+	
+	private Color hoverBackgroundColor;
+	private Color pressedBackgroundColor;
+	
+	public ColorButton() {
+		this(null);
+	}
+	
+	public ColorButton(String text) {
+		super(text);
+		super.setContentAreaFilled(false);
+	}
+	
+	@Override
+    protected void paintComponent(Graphics g) {
+        if (getModel().isPressed()) {
+            g.setColor(pressedBackgroundColor);
+        } else if (getModel().isRollover()) {
+            g.setColor(hoverBackgroundColor);
+        } else {
+            g.setColor(getBackground());
+        }
+        g.fillRect(0, 0, getWidth(), getHeight());
+        super.paintComponent(g);
+    }
+
+    @Override
+    public void setContentAreaFilled(boolean b) {
+    }
+
+    public Color getHoverBackgroundColor() {
+        return hoverBackgroundColor;
+    }
+
+    public void setHoverBackgroundColor(Color hoverBackgroundColor) {
+        this.hoverBackgroundColor = hoverBackgroundColor;
+    }
+
+    public Color getPressedBackgroundColor() {
+        return pressedBackgroundColor;
+    }
+
+    public void setPressedBackgroundColor(Color pressedBackgroundColor) {
+        this.pressedBackgroundColor = pressedBackgroundColor;
+    }
+}
