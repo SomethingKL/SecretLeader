55fdef2e85c754a3895bbf1bbddff610c5db06a5
brewer-slack <bslack@zagmail.gonzaga.edu>
2017-04-23 21:28:33 -0700

    testing

--- SecretLeader/src/JUnitTesting/InitPresTest.java
+++ SecretLeader/src/JUnitTesting/InitPresTest.java
@@ -8,9 +8,12 @@ import framework.TCPClient;
 
 public class InitPresTest {
 	TCPClient client = new TCPClient();
+	
 	@Test
 	public void test() {
-		
+		String[] initPres = client.readFile("data/Turn.txt");
+		String[] firstPlayer = client.readFile("data/Players.txt");
+		assertEquals(initPres[0],firstPlayer[0]);
 	}
 
 }
