cf620e85e50d30f8fb7d214114bb9d3d1163e12f
brewer-slack <bslack@zagmail.gonzaga.edu>
2017-04-23 19:47:50 -0700

    some unit testing classes

---
+++ SecretLeader/src/JUnitTesting/InitPresTest.java
@@ -0,0 +1,16 @@
+package JUnitTesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import framework.TCPClient;
+
+public class InitPresTest {
+	TCPClient client = new TCPClient();
+	@Test
+	public void test() {
+		
+	}
+
+}
--- SecretLeader/src/JUnitTesting/SLTest.java
+++ SecretLeader/src/JUnitTesting/SLTest.java
@@ -4,11 +4,31 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class SLTest {
+import framework.TCPClient;
 
+public class SLTest {
+	TCPClient client = new TCPClient();
+	String nextScreen = "false";
+	
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		System.out.println("reading leaveStarting.txt");
+		String[] next = client.readFile("data/leaveStarting.txt");
+		assertEquals(next[0], nextScreen);
+		
+		System.out.println("reading Board.txt");
+		String[] board = client.readFile("data/Board.txt");
+		assertEquals(board[0], "0");
+		assertEquals(board[1], "0");
+		
+		System.out.println("reading displayInfo.txt");
+		String[] info = client.readFile("data/displayInfo.txt");
+		assertEquals(info[0], "0");
+		
+		System.out.println("reading ProposedChancellor.txt");
+		String[] chance = client.readFile("data/ProposedChancellor.txt");
+		assertEquals(chance[0], "None");
 	}
 
 }
