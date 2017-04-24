c2875242ced1df152420ef40b56be6f799308727
Jacob Krantz <Jacob@Jacob-PC>
2017-04-23 17:25:55 -0700

    added testing suite framework

---
+++ SecretLeader/src/JUnitTesting/TestSuite.java
@@ -0,0 +1,15 @@
+package JUnitTesting;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+// all test classes included in suite go here
+@Suite.SuiteClasses({
+   SLTest.class,
+   //nextTest.class
+})
+
+public class TestSuite {   
+}  
