08bea9f42b4df4a381243a942bbe54723bb2dd59
SomethingKL <rossmbrandt@gmail.com>
2017-04-24 19:48:41 -0700

    server

--- SecretLeader/src/framework/Driver.java
+++ SecretLeader/src/framework/Driver.java
@@ -8,5 +8,6 @@ public class Driver {
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
 		SLFrame start_the_game = new SLFrame();
+		FileHandler f = new FileHandler();
 	}
 }
